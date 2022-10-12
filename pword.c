3c40078c7e4f2765ce3d58a9f3670ec0571325f6
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-10-12 10:47:52 +0300

    Update pword.c

--- pword.c
+++ pword.c
@@ -4,21 +4,43 @@
 
 #include "file_process.h"
 
-int main()
+#define MIN_ARGS 4
+
+int main(int argc, char const *argv[])
 {
-    // for(int i = 0; i < 5; i++)
-    // {  
-    //     pid_t c = fork();
-    //     if(c == 0) 
-    //     { 
-    //         printf("[son] pid %d from [parent] pid %d i = %d\n",getpid(),getppid(), i);
-    //         exit(0);
-    //     } 
-    // }
+    int file_num;
 
-    // for(int i = 0; i < 5; i++)
-    //     wait(NULL);
+    if (argc < MIN_ARGS)
+    {
+        fprintf(stderr, "Argument number is insufficient!");
+        exit(-1);
+    }
+    else
+    {
+        file_num = atoi(argv[3]);
 
-    word_frequency("example.txt");
+        if (argc != MIN_ARGS + file_num)
+        {
+            fprintf(stderr, "File number and existing files are incompatible!");
+            exit(-1);
+        }
+        else
+        {
+            for(int i = 0; i < file_num; i++)
+            {  
+                pid_t n = fork();
+                if(n == 0) // child process execution
+                { 
+                    // call word_frequency for each child process
+                } 
+            }
 
+            for(int i = 0; i < file_num; i++)
+                wait(NULL);
+
+        }
+    }
+    
+
+    return 0;
 }
